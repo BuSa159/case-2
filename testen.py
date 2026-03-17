4db1618a49d2262109a3ec8a8d0f92f9ec7d8d36
BuSa159 <buurman.sander@gmail.com>
2026-03-17 17:06:55 +0100

    de key voor IBM

--- testen.py
+++ testen.py
@@ -1,3 +1,26 @@
 #testen voor git
 
 # nieuwe code voor de toekomst en mergen
+
+
+import pandas as pd
+import requests
+
+# 1. Gebruik je eigen key
+Key = "046SOW0RCBGPECLG"
+
+# 2. Gebruik een f-string (f"...") om de variabele 'Key' in de URL te plaatsen
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={Key}'
+
+r = requests.get(url)
+data = r.json()
+
+# 3. Zet om naar DataFrame en gebruik .T om datums naar de rijen te verplaatsen
+# We gebruiken .from_dict() voor meer controle
+IBM = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient='index')
+
+# Optioneel: zet de index om naar echte datums en de cijfers naar floats
+IBM.index = pd.to_datetime(IBM.index)
+IBM = IBM.astype(float)
+
+print(IBM.head())
