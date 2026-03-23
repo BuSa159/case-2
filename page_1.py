69d63e8ebf1a5a5fc6ab8a56a876d2ab3a56a8d9
BuSa159 <buurman.sander@gmail.com>
2026-03-23 16:12:36 +0100

    Switch to Plotly for volume plotting
    Replaced matplotlib with Plotly for volume visualization and updated layout settings.

--- page_1.py
+++ page_1.py
@@ -1,23 +1,40 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import yfinance as yf
 
 st.title("Page 1")
 st.write("Content for page 1 goes here.")
 
-
 tickers = ['XOM', 'CVX', 'SHEL', 'TTE']
 data = yf.download(tickers, period="1y", group_by='ticker', auto_adjust=True)
 
-fig, ax = plt.subplots(2, 2, figsize=(12, 8))
+fig = make_subplots(
+    rows=2, cols=2,
+    subplot_titles=[f'Volume over tijd voor {t}' for t in tickers]
+)
 
 for i, ticker in enumerate(tickers):
-    row, col = i // 2, i % 2
+    row, col = i // 2 + 1, i % 2 + 1
     volume = data[ticker]['Volume']
-    ax[row, col].plot(volume.index, volume)
-    ax[row, col].set_title(f'Volume over tijd voor {ticker}')
-    ax[row, col].set_xlabel('Tijd')
-    ax[row, col].set_ylabel('Volume')
 
-plt.tight_layout()
-st.pyplot(fig)
+    fig.add_trace(
+        go.Scatter(
+            x=volume.index,
+            y=volume,
+            name=ticker,
+            mode='lines'
+        ),
+        row=row, col=col
+    )
+
+fig.update_layout(
+    height=700,
+    title_text="Volume over tijd",
+    showlegend=False
+)
+
+fig.update_xaxes(title_text="Tijd")
+fig.update_yaxes(title_text="Volume")
+
+st.plotly_chart(fig, use_container_width=True)
