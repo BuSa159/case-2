5c621664bc379f348e90660ce7e3f3c60e1e7d0d
JelmerSch <jelmerschaaphok@gmail.com>
2026-03-23 20:57:28 +0100

    Update Streamlit3.py
    namen

--- Streamlit3.py
+++ Streamlit3.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.set_page_config(layout="wide", page_title="Stock Dashboard")
 
-dashboard = st.Page("dashboard.py", title="Dashboard", icon=":material/dashboard:")
-page_1    = st.Page("page_1.py",   title="Page 1",      icon=":material/article:")
-page_2    = st.Page("page_2.py",   title="Page 2",      icon=":material/article:")
-page_3    = st.Page("page_3.py",   title="Page 3",      icon=":material/article:")
+dashboard = st.Page("dashboard.py", title="Startpagina",           icon=":material/dashboard:")
+page_1    = st.Page("page_1.py",   title="Dividend analyse",       icon=":material/article:")
+page_2    = st.Page("page_2.py",   title="Bedrijfsinfo",           icon=":material/article:")
+page_3    = st.Page("page_3.py",   title="Aandelen voorspeller",   icon=":material/article:")
 
 pg = st.navigation([dashboard, page_1, page_2, page_3])
 pg.run()
