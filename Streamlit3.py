b106d682a10f26242dbb4222d677aca29a68098d
JelmerSch <jelmerschaaphok@gmail.com>
2026-03-23 20:59:22 +0100

    Update Streamlit3.py
    volgorde

--- Streamlit3.py
+++ Streamlit3.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title="Stock Dashboard")
 
 dashboard = st.Page("dashboard.py", title="Startpagina",           icon=":material/dashboard:")
-page_1    = st.Page("page_2.py",   title="Dividend analyse",       icon=":material/article:")
-page_2    = st.Page("page_1.py",   title="Bedrijfsinfo",           icon=":material/article:")
+page_1    = st.Page("page_2.py",   title="Bedrijfsinfo",           icon=":material/article:")
+page_2    = st.Page("page_1.py",   title="Dividend analyse",       icon=":material/article:")
 page_3    = st.Page("page_3.py",   title="Aandelen voorspeller",   icon=":material/article:")
 
 pg = st.navigation([dashboard, page_1, page_2, page_3])
