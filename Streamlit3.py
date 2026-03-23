ef33e6dcc00c47ea88ffe11ea277d784728e8c96
JelmerSch <jelmerschaaphok@gmail.com>
2026-03-23 20:29:18 +0100

    Update Streamlit3.py

--- Streamlit3.py
+++ Streamlit3.py
@@ -7,5 +7,5 @@ page_1    = st.Page("page_1.py",   title="Page 1",      icon=":material/article:
 page_2    = st.Page("page_2.py",   title="Page 2",      icon=":material/article:")
 page_3    = st.Page("page_3.py",   title="Page 3",      icon=":material/article:")
 
-pg = st.navigation([dashboard, page_1, page_2])
+pg = st.navigation([dashboard, page_1, page_2, page_3])
 pg.run()
