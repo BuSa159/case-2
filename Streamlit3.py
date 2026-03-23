3e46cce495bb970e0483182d636bd4569e53f91f
JelmerSch <jelmerschaaphok@gmail.com>
2026-03-23 20:26:39 +0100

    Update Streamlit3.py
    extra pagina

--- Streamlit3.py
+++ Streamlit3.py
@@ -5,6 +5,7 @@ st.set_page_config(layout="wide", page_title="Stock Dashboard")
 dashboard = st.Page("dashboard.py", title="Dashboard", icon=":material/dashboard:")
 page_1    = st.Page("page_1.py",   title="Page 1",      icon=":material/article:")
 page_2    = st.Page("page_2.py",   title="Page 2",      icon=":material/article:")
+page_3    = st.Page("page_3.py",   title="Page 3",      icon=":material/article:")
 
 pg = st.navigation([dashboard, page_1, page_2])
 pg.run()
