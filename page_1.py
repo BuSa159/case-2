eb88353ebcea0a8b8f5cbc7f98fbb4ffe25625c7
BuSa159 <buurman.sander@gmail.com>
2026-03-23 16:10:27 +0100

    Update page_1.py

--- page_1.py
+++ page_1.py
@@ -1,17 +1,23 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import yfinance as yf
 
 st.title("Page 1")
 st.write("Content for page 1 goes here.")
 
-# Probeersel vanuit co pilot  
-#  Tweede grafiek voor volume over tijd van verschillende aandelen
-fig, ax = plt.subplots(5, 2)
-for i, ticker in enumerate(['XOM', 'CVX', 'SHEL', 'TTE']):
-    ax[i//2, i%2].plot(df[df['ticker'] == ticker].index, df[df['ticker'] == ticker]['volume'])
-    ax[i//2, i%2].set_title(f'Volume over tijd voor {ticker}')
-    ax[i//2, i%2].set_xlabel('Tijd')
-    ax[i//2, i%2].set_ylabel('Volume')
+
+tickers = ['XOM', 'CVX', 'SHEL', 'TTE']
+data = yf.download(tickers, period="1y", group_by='ticker', auto_adjust=True)
+
+fig, ax = plt.subplots(2, 2, figsize=(12, 8))
+
+for i, ticker in enumerate(tickers):
+    row, col = i // 2, i % 2
+    volume = data[ticker]['Volume']
+    ax[row, col].plot(volume.index, volume)
+    ax[row, col].set_title(f'Volume over tijd voor {ticker}')
+    ax[row, col].set_xlabel('Tijd')
+    ax[row, col].set_ylabel('Volume')
 
 plt.tight_layout()
-plt.show()
+st.pyplot(fig)
